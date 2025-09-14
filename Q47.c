36bf4ac1da555670d04b00103e4dbe31c347bd77
Swastik <swastik292007@gmail.com>
2025-09-14 11:47:37 +0530

    Add programs for star and number patterns
    Added Q47.c to print a left-aligned triangle of stars and Q48.c to print a left-aligned triangle of increasing numbers. Corresponding compiled executables Q47.exe and Q48.exe are also included.

---
+++ Q47.c
@@ -0,0 +1,21 @@
+/*Write a program to print the following pattern:
+*
+**
+***
+****
+*****     */
+#include <stdio.h>
+
+int main()   {
+    int n,i,j;
+    printf("enter number of rows:");
+    scanf("%d",&n);
+
+    for(i=1 ; i<=n ; i++) {
+        for(j=1 ; j<=i ; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Q48.c
@@ -0,0 +1,20 @@
+/*Write a program to print the following pattern:
+1
+12
+123
+1234
+12345    */
+#include <stdio.h>
+int main() {
+    int n,i,j;
+    printf("enter number of rows: ");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=i;j++) {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
