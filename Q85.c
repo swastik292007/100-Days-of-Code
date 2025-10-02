b77f3693db932942cc5722fa71aa7f0b9da21944
Swastik <swastik292007@gmail.com>
2025-10-02 22:49:36 +0530

    Add new C source files

---
+++ Q85.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int len, i;
+
+    scanf("%s", str);   // Input (single word as per sample test case)
+
+    len = strlen(str);
+
+    // Swapping characters from start and end
+    for (i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+
+    printf("%s\n", str);
+    return 0;
+}
